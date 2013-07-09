c1ae87727d835f9f3ee46101559705048cf85d1a
daJu <julien.delaigues@gmail.com>
2013-07-09 02:20:25 +0200

    Removing bugged chars and addins missing preproc clause

--- ext/epitech/bitop256.h
+++ ext/epitech/bitop256.h
@@ -1,5 +1,9 @@
-void ClearAll256(unsigned char *bits);
-int TestBit256(const unsigned char *bits, unsigned char bit);
-void Copy256(unsigned char *dest, const unsigned char *src);
-void LeftShift256(unsigned char *bits, int shifts);
-void RightShift256(unsigned char *bits, int shifts);
+#ifndef		__BITOP256_H
+#define		__BITOP256_H
+ void ClearAll256 (unsigned char *bits);
+int TestBit256 (const unsigned char *bits, unsigned char bit);
+void Copy256 (unsigned char *dest, const unsigned char *src);
+void LeftShift256 (unsigned char *bits, int shifts);
+void RightShift256 (unsigned char *bits, int shifts);
+ 
+#endif /*  */
