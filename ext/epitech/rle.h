0afc37b63ba32ea1f422396c226e189770513b3f
daJu <julien.delaigues@gmail.com>
2013-07-13 12:37:24 +0200

    RLE working with size

--- ext/epitech/rle.h
+++ ext/epitech/rle.h
@@ -1,8 +1,7 @@
 #ifndef		__RLE_H
 #define		__RLE_H
 
-
-void rle_encode (unsigned char *in, unsigned char *out);
+void rle_encode (unsigned char *in, unsigned char *out, unsigned int length);
 void rle_decode (unsigned char *in, unsigned char *out);
 
 #endif
