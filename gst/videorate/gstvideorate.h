96880d966e47b3205db852667f58f5985668b046
Thibault Saunier <tsaunier@gnome.org>
2014-12-09 13:18:42 +0100

    videorate: Detect framerate if not forced to variable downstream
    In case upstream does not provide videorate with framerate information,
    it will detect the current framerate from the buffer it received,
    but if downstream forces the use of variable framerate (most probably
    through the use of a caps filter with framerate = 0 / 1), videorate will
    respect that.
    
    https://bugzilla.gnome.org/show_bug.cgi?id=734424

--- gst/videorate/gstvideorate.h
+++ gst/videorate/gstvideorate.h
@@ -65,6 +65,7 @@ struct _GstVideoRate
   guint64 average_period;
   GstClockTimeDiff wanted_diff; /* target average diff */
   GstClockTimeDiff average;     /* moving average period */
+  gboolean force_variable_rate;
 
   /* segment handling */
   GstSegment segment;
